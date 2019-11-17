964bb6753e7e91d4bfea5db8b61ecb2ae8269766
BK1918HSK <1220266776@qq.com>
2019-11-17 21:16:59 +0800

    ok

--- src/api/myadress.js
+++ src/api/myadress.js
@@ -57,7 +57,6 @@ export const venue=(id)=>http({
 export const search=(search)=>http({
     method:"get",
     url:'/server/search/s/'+search+'.json',
-<<<<<<< HEAD
     // 搜索页请求接口
 })
 
@@ -81,9 +80,7 @@ export const CT=()=>http({
     // 城市页面请求接口   
     // https://m.228.cn/server/content/city/list.json})
 })
-=======
   
-})
 
 
 export const searchusername=(username,password)=>http({
@@ -108,7 +105,3 @@ export const getaxios=(username,password)=>http({
         password:password,
     }
 })
-<<<<<<< HEAD
-=======
->>>>>>> 更新登录注册
->>>>>>> master
